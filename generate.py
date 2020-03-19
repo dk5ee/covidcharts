56a5a2f2e20767af609160e1cb649166be66b1e7
dk5ee <ernstpeterboehm@gmail.com>
2020-03-19 11:04:13 +0000

    seems to work now

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 import matplotlib as mpl
+# allow using mpl without running x server
 mpl.use('Agg')
-import matplotlib.pyplot as mysheet
+import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 import csv
@@ -8,31 +9,30 @@ import datetime
 import sys
 
 
-def plotcountry(name, comment, da, va, de, re):
+def plotcountry(name, comment, dates, infected, deceased, recovered):
 	# clean up filename
-	dates = da
-	values = np.array(va)
-	deceaseds = np.array(de)
-	recovered = np.array(re)
 	filename = "".join([c for c in name if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
 
-	with mysheet.xkcd():
-		fig = mysheet.figure()
-		mysheet.plot(dates, values)
-		mysheet.plot(dates, recovered)
-		mysheet.plot(dates, deceaseds)
-		mysheet.legend(['Confirmed ' + str(values[-1]), 'Recovered ' + str(recovered[-1]), 'Deceased ' + str(deceaseds[-1])], loc=2)
-		mysheet.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-		mysheet.gca().xaxis.set_major_locator(mdates.DayLocator(interval=5))
-		mysheet.gcf().autofmt_xdate()
-		mysheet.title(name + " " + comment + "\n" + str(datetime.datetime.now().date()))
-		mysheet.savefig('charts/' + filename + '_small.png', dpi=90)
-		mysheet.savefig('charts/' + filename + '_300dpi.png', dpi=300)
-		mysheet.close(fig);
-	# with open('charts/' + filename + '.csv', mode='w') as csv_file:
-	# 	csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	# 	for item in da:
-	# 		csv_writer.writerow([item.date(), va.pop(0), re.pop(0), de.pop(0)])
+	with plt.xkcd():
+		fig = plt.figure()
+		plt.plot(dates, infected)
+		plt.plot(dates, recovered)
+		plt.plot(dates, deceased)
+		plt.legend(['Confirmed ' + str(infected[-1]), 'Recovered ' + str(recovered[-1]), 'Deceased ' + str(deceased[-1])], loc=2)
+		plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+		plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=5))
+		plt.gcf().autofmt_xdate()
+		plt.title(name + " " + comment + "\nlatest data from " + str(dates[-1].date()))
+		plt.savefig('charts/' + filename + '_small.png', dpi=90)
+		plt.savefig('charts/' + filename + '_300dpi.png', dpi=300)
+		plt.close(fig)
+	with open('charts/' + filename + '.csv', mode='w') as csv_file:
+		csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		for item in dates:
+			infectedvalue, infected = infected[0], infected[1:]
+			deceasedvalue, deceased = deceased[0], deceased[1:]
+			recoveredvalue, recovered = recovered[0], recovered[1:]
+			csv_writer.writerow([item.date(), infectedvalue, recoveredvalue, deceasedvalue])
 	return
 
 
@@ -46,62 +46,61 @@ infectedfilename = 'COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_s
 
 deceasedfile = open(deceasedfilename, newline='')
 recoveredfile = open(recoveredfilename, newline='')
-csvfile = open(infectedfilename, newline='')
+infectedfile = open(infectedfilename, newline='')
 
 deceaseddata = csv.reader(deceasedfile, delimiter=',', quotechar='"')
-#drop first row, the header
+# drop first row, the header
 next(deceaseddata)
 recovereddata = csv.reader(recoveredfile, delimiter=',', quotechar='"')
-#drop first row, the header
+# drop first row, the header
 next(recovereddata)
 
-infecteddata = csv.reader(csvfile, delimiter=',', quotechar='"')
+infecteddata = csv.reader(infectedfile, delimiter=',', quotechar='"')
 
 header = next(infecteddata)
 header = header[4:]
-
-dates = [];
-
-
+dates = []
 
 for data in header:
-	#header contains datetimes
+	# header contains datetimes
 	datetimeobj = datetime.datetime.strptime(data, '%m/%d/%y')
 	dates.append (datetimeobj)
 for infected in infecteddata:
 	deceased = next(deceaseddata)
 	deceased = deceased[4:]
-	deceased = list(map(int, deceased))
+	deceasedvalues = np.array(list(map(int, deceased)))
 	recovered = next(recovereddata)
 	recovered = recovered[4:]
-	recovered = list(map(int, recovered))
-	state = infected.pop(0)
+	recoveredvalues = np.array(list(map(int, recovered)))
+	state = infected.pop(0).strip()
 	region = infected.pop(0).strip()
 	name = region
-	lat = infected.pop(0)
-	long = infected.pop(0)
+	lat = infected.pop(0).strip()
+	long = infected.pop(0).strip()
 	
-	infectedvalues = list(map(int, infected))
-	# TODO:
-	# aggregation of regions does not work all times
+	infectedvalues = np.array(list(map(int, infected)))
+	# print(name + " " + str(infectedvalues.shape) + " " + str(deceasedvalues.shape) + " " + str(recoveredvalues.shape))
 	if state:
+		# if string for "state" is set, the values of the region are split and need to be aggregated
 		name = region + " " + state
 		if region in regionsinfected.keys():
 			olddata = regionsinfected[region]
 			oldddata = regionsdeceased[region]
 			oldrdata = regionsrecovered[region]
-			newdata = [x + y for x, y in zip(infectedvalues, olddata)]
-			newddata = [x + y for x, y in zip(deceased, oldddata)]
-			newrdata = [x + y for x, y in zip(recovered, oldrdata)]
+			newdata = np.add(infectedvalues, olddata)
+			newddata = np.add(deceasedvalues, oldddata)
+			newrdata = np.add(recoveredvalues, oldrdata)
 			regionsinfected[region] = newdata
 			regionsdeceased[region] = newddata
 			regionsrecovered[region] = newrdata
 		else:
 			regionsinfected[region] = infectedvalues
-			regionsdeceased[region] = deceased
-			regionsrecovered[region] = recovered
-	# disable plotting of countries for testing
-	#plotcountry (name, "CoVid 19 cases", dates, values, deceaseds, recovered)
+			regionsdeceased[region] = deceasedvalues
+			regionsrecovered[region] = recoveredvalues
+	plotcountry (name, "CoVid 19 cases", dates, infectedvalues, deceasedvalues, recoveredvalues)
 for region in regionsinfected.keys():
-		
-	plotcountry (region, "CoVid 19 cases", dates, regionsinfected[region], regionsdeceased[region], regionsrecovered[region])
+	infectedvalues = regionsinfected[region]
+	deceasedvalues = regionsdeceased[region]
+	recoveredvalues = regionsrecovered[region]
+	# print(name + " " + str(infectedvalues.shape) + " " + str(deceasedvalues.shape) + " " + str(recoveredvalues.shape))
+	plotcountry (region, "CoVid 19 cases", dates, infectedvalues, deceasedvalues, recoveredvalues)
